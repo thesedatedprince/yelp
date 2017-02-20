b6a48ebb83a7aa7e812a6a06cf54d7d9d7b9b652
PeteGH <peter@grant-hay.eu>
2017-02-20 18:59:36 +0100

    adds thoughts and reviews model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews
+  end 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'restaurants' => 'restaurants#index'
+
 end
