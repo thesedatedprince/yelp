49231ca9e42b2f684ebbca8aa189e4134f23c4fc
PeteGH <peter@grant-hay.eu>
2017-02-22 14:21:37 +0100

    adds user reg and sign in and out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root "restaurants#index"
   resources :restaurants do
     resources :reviews
-  end 
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'restaurants' => 'restaurants#index'
 
